f1eb4c4c88cafb86f39cbc4befdaa66784f16704
Benjamin Sergent <benjamin.sergent@daqri.com>
2016-12-20 20:49:28 -0800

    Made test threading scene

---
+++ Assets/Scripts/CountOnThread.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+using UnityEngine.UI;
+
+public class CountOnThread : MonoBehaviour {
+	Thread countThread;
+	Text text;
+	bool done = false;
+	// Use this for initialization
+	void Start () {
+		text = GetComponent<Text> ();
+		countThread = new Thread (new ThreadStart (ThreadLoop));
+		countThread.Start ();
+	}
+
+	void OnDestroy() {
+		done = true;
+	}
+
+	void ThreadLoop() {
+		int cnt = 0;
+		while(!done) {
+			cnt++;
+			Dispatcher.Instance.dispatch(() => text.text = "" + cnt);
+
+			Thread.Sleep (500);
+			Debug.Log ("Cnt is " + cnt);
+		}
+
+	}
+
+}
---
+++ Assets/Scripts/Dispatcher.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dispatcher : MonoBehaviour {
+	public static Dispatcher Instance {
+		get {
+			return _instance;
+		}
+	}
+		
+	static private Dispatcher _instance;
+	private List<System.Action> _workList;
+
+	void Awake() {
+		_workList = new List<System.Action> ();
+		_instance = this;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		foreach(System.Action work in _workList) {
+			work ();
+		}
+
+		_workList.Clear ();
+	}
+
+	public void dispatch(System.Action work) {
+		_workList.Add (work);
+	}
+}
